d6d7ced54c428ace9dccf9a1a33891c13259a73b
Rudy Sanchez <rudys212@gmail.com>
2021-04-07 00:53:01 -0700

    added new variables

--- script.js
+++ script.js
@@ -4,10 +4,8 @@ var generateBtn = document.getElementById("generate");
 
 var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var specialCharacters = ["\""," ","!","#","$","%","&","'","(",")","*","+","-",".","/",":",";","<","=",">","?","@","[","\", "]","^","_","`", "{", "|","}","~"]
-var numericCharacters
-
-
+var specialCharacters = ["!","\"","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\\","]","^","_","`","{","|","}","~"]
+var numericCharacters = ["0","1","2","3","4","5","6","7","8","9"]
 
 
 // Write password to the #password input
