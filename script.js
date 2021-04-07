08e575454725b61a97ac0b4d6c581b9566a7f2cc
Rudy Sanchez <rudys212@gmail.com>
2021-04-07 00:51:39 -0700

    added new variables in javascript file

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+//Setting up variables
+var generateBtn = document.getElementById("generate");
+
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var specialCharacters = ["\""," ","!","#","$","%","&","'","(",")","*","+","-",".","/",":",";","<","=",">","?","@","[","\", "]","^","_","`", "{", "|","}","~"]
+var numericCharacters
+
+
+
 
 // Write password to the #password input
 function writePassword() {
